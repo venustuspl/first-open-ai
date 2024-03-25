3db50f738bc4d884eeed689d33a3c79de08bcc81
tom <venustus.pl@gmail.com>
2024-03-25 11:40:43 +0100

    new name file

--- start.py
+++ start.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import os
 
 from pathlib import Path
@@ -5,17 +6,15 @@ import fire
 from openai import OpenAI
 
 from dotenv import load_dotenv
+
+def get_timestamp():
+    return datetime.now().strftime("%Y%b%d_%H-%M")
 def save_output_to_file(
     out_dir,
     output,
     file_name,
 ):
-    """
-    Saves the generated output to a file.
-    :param out_dir: The output directory.
-    :param output: The text to be saved.
-    :param file_name: The name of the output file.
-    """
+
     out_dir = Path(out_dir)
     out_dir.mkdir(parents=True, exist_ok=True)
     output_file = out_dir / file_name
@@ -47,10 +46,12 @@ def main():
         frequency_penalty=0.1
     )
 
+    ts = get_timestamp()
+    output_file_name = ts + "_response.txt"
     model_response = chat_completion.choices[0].message.content
     print(model_response)
     current_directory = os.getcwd()
-    save_output_to_file(current_directory, model_response, 'response.txt')
+    save_output_to_file(current_directory, model_response, output_file_name)
 
 if __name__ == "__main__":
     fire.Fire(main)
